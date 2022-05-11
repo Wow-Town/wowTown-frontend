ce49ef6a3b9d7a13df5588b3c69fa1e2e467380d
nayeonLeee <sh05060@naver.com>
2022-05-11 20:13:56 +0900

    issue-20-input-component

---
+++ src/components/InputInfo.js
@@ -0,0 +1,11 @@
+import './InputInfo.css';
+
+export default function InputInfo({maxLength, label, inputType,value,onChange}){
+   //label="이메일 주소"inputType="email" value onChange
+    return(
+        <div className="inputInfo">
+            <label className="inputLabel">{label}</label>
+            <input className="inputFrame" type={inputType} defaultValue={value} onChange={onChange} maxLength={maxLength}></input>
+        </div>
+    );
+}
