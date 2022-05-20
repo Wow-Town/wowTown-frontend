f5a1be11c6448f25f993af185a9c6e02b5183c0c
nayeonLeee <98935485+nayeonLeee@users.noreply.github.com>
2022-05-20 11:59:00 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
 import './App.css';
+import Login from './pages/Login';
+import Join from './pages/Join';
+import Home from './pages/Home';
+import { BrowserRouter , Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+   <BrowserRouter >
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/users/signup" element={<Join/>} />
+        <Route path="/users/login" element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
