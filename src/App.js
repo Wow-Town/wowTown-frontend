3cc43ee11e57b0935a6ca25ec70478ed917aaf34
nayeonLeee <sh05060@naver.com>
2022-06-21 18:29:25 +0900

    .

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import Join from './pages/Join';
 import Home from './pages/Home';
 import { BrowserRouter , Routes, Route } from "react-router-dom";
 
-
 function App() {
   return (
    <BrowserRouter >
       
       <Routes>
-        <Route index element={<Home />} />
+        <Route index element={<Home/>} />
         <Route path="/signup" element={<Join/>} />
         <Route path="/login" element={<Login/>} />
         
