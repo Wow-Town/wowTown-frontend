baafeefcffca4be49e81618b063ff69839ce9ba0
nayeonLeee <sh05060@naver.com>
2022-06-21 22:42:17 +0900

    임시 채널

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,23 +1,60 @@
 import styled from "styled-components";
-import Button from "../components/Button";
+import Button from "./Button";
 
 
 export default function Channel({channelList}){
     return(
-        <DivChannel>
-            
-            <InfoChannel></InfoChannel>
-            <Button buttonText="입장"/>
-            
-        </DivChannel>
+        <DivChannelList>
+           {channelList.map( channel =>{
+            return(
+                <ChannelFrame key={channel.channelId}>
+                <DivChannel>
+                <InfoChannel>
+                    채널1
+                    <div>00/20명</div>
+                </InfoChannel>
+                <Button buttonText="입장"/>
+                </DivChannel>
+                </ChannelFrame>
+            )
+            })}
+        </DivChannelList>
     );
 
 }
 
+const DivChannelList=styled.div`
+    display:flex;
+    flex-direction: column; 
+    justify-content: center; 
+    padding-left:83px;
+`
+
+const ChannelFrame=styled.div`
+
+    
+`
 const DivChannel=styled.div`
-
+    display:flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    border: 1px solid #A4A4A4 ;
+    border-radius: 10px;
+    height:100px;
+    width:510px;
+    
+    
 `
 
-const InfoChannel=styled.div`
-
+const InfoChannel=styled.span`
+    display:flex;
+    flex-direction: column; 
+    width: 100px;
+    padding-right:170px;
+    font-size:16px;
+    
 `
+
+
+
--- src/pages/Channels.js
+++ src/pages/Channels.js
@@ -24,16 +24,20 @@ export default function Channels(){
     return(
         <ContentsFrame>
             <Header text="입장할 채널을 고르세요"/>
-            <ChannelList>
-                <Channel channelList={channelList}/>
-            </ChannelList>
+            <Channel channelList={channelList}/>
         </ContentsFrame>
     );
 }
 
 const ContentsFrame = styled.div`
+    display:flex;
+    flex-direction: column;
+    height:100%;
+    width:670px;
 `
 
 const ChannelList = styled.div`
+    display:flex;
+    flex-direction: column;
 
 `
