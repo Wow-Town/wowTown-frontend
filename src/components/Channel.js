52c75a8891033b79c6c756c2d79ac66479215a34
nayeonLeee <sh05060@naver.com>
2022-06-23 00:07:43 +0900

    빌드 오류 수정

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Button from "./Button";
+import Button from "./Button.js";
 import {useNavigate} from 'react-router-dom';
 
 export default function Channel({channelList}){
