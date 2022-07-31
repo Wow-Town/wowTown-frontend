043d01a832fdb1d6326733715caec9ded979eb25
nayeonLeee <sh05060@naver.com>
2022-08-01 01:21:03 +0900

    공고 post

---
+++ src/components/Frame.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export default function Frame(){
+    return(
+        <Div>
+
+        </Div>
+    )
+
+}
+
+const Div = styled.div`
+    padding : 20px 30px 20px 30px;
+    border: 1px solid #A4A4A4 ;
+    width:490px;
+`
