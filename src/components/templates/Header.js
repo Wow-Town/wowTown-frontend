5782c54ed8b4fb7d1568a3f9936c5dae7205ac14
nayeonLeee <sh05060@naver.com>
2022-08-02 18:10:46 +0900

    rm classname

--- src/components/templates/Header.js
+++ src/components/templates/Header.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 export default function Header({text}){
     return(
         <ContentsHeader>
-            <Logo classname="logo"/>
+            <Logo/>
             <ContenetsHeaderTitle>{text}</ContenetsHeaderTitle>
         </ContentsHeader>
 
