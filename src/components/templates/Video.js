5431b784e3257c2160ccc3f746f7f42fcd10e1a6
Hyunsik Kim <devconf5296@gmail.com>
2022-11-06 21:04:39 +0900

    Remove audio howling

--- src/components/templates/Video.js
+++ src/components/templates/Video.js
@@ -1,14 +1,26 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import styled from "styled-components";
+import { AvatarState } from "../../utils/AvatarState";
+import { useRecoilValue } from 'recoil';
 import { useRef ,useEffect } from 'react';
 
 export default function Video({id,stream}) {
+
+    const avatar = useRecoilValue(AvatarState);
+
     const viewRef = useRef(null);
 
   useEffect(() => {
-    if (!viewRef.current)
+    if (!viewRef.current){
         return;
+    }
+    console.log(avatar.avatarId);
+    console.log(id);
+    if(avatar.avatarId.toString() === id){
+        console.log(viewRef);
+        viewRef.current.volume = 0;
+    }
     viewRef.current.srcObject = stream ? stream : null;
   }, [stream]);
 
